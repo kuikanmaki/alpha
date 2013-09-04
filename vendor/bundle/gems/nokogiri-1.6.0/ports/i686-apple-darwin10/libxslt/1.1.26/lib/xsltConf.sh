f17e4d29b024e359381519dc1f5ff8877a6ae772
Jari Kuikanmäki <jari.kuikanmaki@helsinki.fi>
2013-09-04 21:41:46 +0100

    bundle update after the sass-rails change

---
+++ vendor/bundle/gems/nokogiri-1.6.0/ports/i686-apple-darwin10/libxslt/1.1.26/lib/xsltConf.sh
@@ -0,0 +1,7 @@
+#
+# Configuration file for using the xslt library
+#
+XSLT_LIBDIR="-L/Users/Jari/Desktop/alpha2/vendor/bundle/gems/nokogiri-1.6.0/ports/i686-apple-darwin10/libxslt/1.1.26/lib"
+XSLT_LIBS="-lxslt  -L/Users/Jari/Desktop/alpha2/vendor/bundle/gems/nokogiri-1.6.0/ports/i686-apple-darwin10/libxml2/2.8.0/lib -lxml2 -lz -lpthread -liconv -lm "
+XSLT_INCLUDEDIR="-I/Users/Jari/Desktop/alpha2/vendor/bundle/gems/nokogiri-1.6.0/ports/i686-apple-darwin10/libxslt/1.1.26/include"
+MODULE_VERSION="xslt-1.1.26"
